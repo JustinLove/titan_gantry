643cce48a0d41ca4a9c72bd37785be8168497208
Justin Love <git@JustinLove.name>
2015-11-16 16:53:53 -0600

    catalyst model

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,20 +52,49 @@ module.exports = function(grunt) {
       titan_gantry: {
         src: [
           'pa_ex1/units/land/vehicle_factory_adv/vehicle_factory_adv.json',
+          'pa/units/land/control_module/control_module.json',
         ],
         cwd: media,
         dest: 'pa/units/land/titan_gantry/titan_gantry.json',
-        process: function(spec) {
+        process: function(fac, cm) {
+          spec = {}
+
+          spec.base_spec = fac.base_spec
           spec.display_name = 'Titan Gantry'
           spec.description = 'Advanced manufacturing- Builds most titans.'
-          spec.si_name = 'metal_storage_adv'
-          spec.buildable_types = "Mobile & Titan"
-          spec.placement_size = [60, 100]
-          spec.unit_types = spec.unit_types.filter(function(type) {
+          spec.build_metal_cost = 15000
+          spec.unit_types = fac.unit_types.filter(function(type) {
             return type != 'UNITTYPE_Tank'
           })
           spec.unit_types.push('UNITTYPE_FabAdvBuild')
+          spec.max_health = fac.max_health
+          spec.recon = fac.recon
+          spec.spawn_layers= "WL_AnyHorizontalGroundOrWaterSurface"
+
+          spec.buildable_types = "Mobile & Titan"
+          spec.rolloff_dirs = [
+            [ 1, 0, 0 ],
+            [ -1, 0, 0 ]
+          ]
+          spec.factory_cool_down_time = fac.factory_cool_down_time
+          spec.wait_to_rolloff_time = fac.wait_to_rolloff_time
+          spec.tools = fac.tools
           spec.tools[0].spec_id = '/pa/units/land/titan_gantry/titan_gantry_tool_build_arm.json'
+          spec.command_caps = fac.command_caps
+          spec.audio = fac.audio
+
+          spec.si_name = 'metal_storage_adv'
+          spec.selection_icon = cm.selection_icon
+          spec.TEMP_texelinfo = cm.TEMP_texelinfo
+          spec.events = cm.events
+          spec.fx_offsets = cm.fx_offsets
+          spec.headlights = cm.headlights
+          spec.lamps = cm.lamps
+          spec.mesh_bounds = cm.mesh_bounds
+          spec.model = cm.model
+          spec.placement_size = cm.placement_size
+          spec.area_build_separation = fac.area_build_separation
+
           return spec
         }
       },
